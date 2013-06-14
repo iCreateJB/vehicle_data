b93d7f95f766ec26f40795d9498bec083501834e
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-06-14 16:18:51 -0400

    Stub configuration block.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,9 @@ require 'vehicle_data' # and any other gems you need
 RSpec.configure do |config|
   config.order = "random"
 end
+
+# Stub configuration ( This will naturally be held in a configuration file )
+VehicleData.configure do |config|
+  config.app_key = "#{SecureRandom.hex(10)}"
+  config.secret  = "#{SecureRandom.hex(15)}"
+end
