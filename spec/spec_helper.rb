e9b6c9b524ddc0a89090b8cc342d3fa5867e1fc1
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-06-13 22:20:56 -0400

    Adding secure random for test helpers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
-
+require 'securerandom'
 require 'vehicle_data' # and any other gems you need
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = false
-  config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 end
